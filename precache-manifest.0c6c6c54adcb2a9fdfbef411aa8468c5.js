7a5382a75b9ab6b3ebce8d29e2c5a86ce7be099f
Shaun van Ardenne <s.vanardenne@gmail.com>
2020-10-28 10:38:09 -0700

    Updates

---
+++ precache-manifest.0c6c6c54adcb2a9fdfbef411aa8468c5.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9633dab110d46c54a9d12b3d02c7da6",
+    "url": "/elder-gods-brewing/index.html"
+  },
+  {
+    "revision": "2d99283a4d6b731655bb",
+    "url": "/elder-gods-brewing/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "5544df62186346a5b41d",
+    "url": "/elder-gods-brewing/static/css/main.210ffd74.chunk.css"
+  },
+  {
+    "revision": "2d99283a4d6b731655bb",
+    "url": "/elder-gods-brewing/static/js/2.88312ec2.chunk.js"
+  },
+  {
+    "revision": "78fd0f62364854e39c53711d027f73a3",
+    "url": "/elder-gods-brewing/static/js/2.88312ec2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5544df62186346a5b41d",
+    "url": "/elder-gods-brewing/static/js/main.ed50891f.chunk.js"
+  },
+  {
+    "revision": "6214c859365356cd4c22",
+    "url": "/elder-gods-brewing/static/js/runtime-main.290bdd71.js"
+  },
+  {
+    "revision": "19be3e70848462ed90d56abc67c6ba2c",
+    "url": "/elder-gods-brewing/static/media/bartender-andriyko-podilnyk.19be3e70.jpg"
+  },
+  {
+    "revision": "265b192ae2ea64d5e775af613fce155a",
+    "url": "/elder-gods-brewing/static/media/cheers-elevate.265b192a.jpg"
+  },
+  {
+    "revision": "5d2fdb45efb9fc073a308b079f47b39f",
+    "url": "/elder-gods-brewing/static/media/hops-andre-klimke.5d2fdb45.jpg"
+  },
+  {
+    "revision": "dbb32abbadbadc08f2b1506ad357a5a1",
+    "url": "/elder-gods-brewing/static/media/hops-elevate.dbb32abb.jpg"
+  },
+  {
+    "revision": "d8b91f7face38461ed75afb465bc75ea",
+    "url": "/elder-gods-brewing/static/media/man-at-bar.d8b91f7f.jpg"
+  },
+  {
+    "revision": "2f124ff6044e4bebb1033585aca5d527",
+    "url": "/elder-gods-brewing/static/media/man-holding-beer-evan-dvorkin.2f124ff6.jpg"
+  },
+  {
+    "revision": "d41d52d77f47e6f56c474c6fe2f9c7e3",
+    "url": "/elder-gods-brewing/static/media/man-in-brewery.d41d52d7.jpg"
+  },
+  {
+    "revision": "f2b02a0854e56b81d2795149cbbaf5c0",
+    "url": "/elder-gods-brewing/static/media/man-in-polo-foto-sushi.f2b02a08.jpg"
+  }
+]);
